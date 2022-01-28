1ac067daaaa0e0dd2826556b21dbdb98fe743141
unknown <sanctity2k5@gmail.com>
2022-01-28 01:58:40 +0100

    Changes to new portfolio layout

--- src/Components/AboutItems.js
+++ src/Components/AboutItems.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import BeenhereSharpIcon from '@material-ui/icons/BeenhereSharp';
-import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { Animated } from "react-animated-css";
